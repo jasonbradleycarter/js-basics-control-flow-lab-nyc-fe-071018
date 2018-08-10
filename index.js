70debc237671a69448f93cd3cc45e28d2d49456c
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-10 00:14:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function ternaryCheckCity(city) {
   }
 }
 
-// function switchOnCharmFromTip(tip)
-//   if (tip = 'generous') {
-//     return 'Thank you so much.';
-//   }
-//
+function switchOnCharmFromTip(tip)
+  if (tip = 'generous') {
+    return 'Thank you so much.';
+  }
+
