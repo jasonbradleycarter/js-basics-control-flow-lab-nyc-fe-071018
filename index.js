06cee024f5a741445c1f9dc582c99886288237e4
Jason Carter <jasonbradleycarter@gmail.com>
2018-08-10 00:00:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function scuberGreetingForFeet(feet) {
     return 'This one is on me!';
   }
   else if (feet > 1999 && feet < 2500) {
-    return 'I will gladly take your thirty bucks';
+    return 'I will gladly take your thirty bucks.';
   }
   else {
     return "No can do.";
